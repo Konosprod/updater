593be0e90e12e79f506d1ddec76507eca624ff3d
Konosprod <konosprod@free.fr>
2013-11-01 12:35:16 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelPerc = new System.Windows.Forms.Label();
+            this.labelDownloaded = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(337, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(56, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(248, 214);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(12, 38);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(7, 13);
+            this.labelSpeed.TabIndex = 3;
+            this.labelSpeed.Text = "\r\n";
+            // 
+            // labelPerc
+            // 
+            this.labelPerc.AutoSize = true;
+            this.labelPerc.Location = new System.Drawing.Point(167, 55);
+            this.labelPerc.Name = "labelPerc";
+            this.labelPerc.Size = new System.Drawing.Size(21, 13);
+            this.labelPerc.TabIndex = 4;
+            this.labelPerc.Text = "0%";
+            // 
+            // labelDownloaded
+            // 
+            this.labelDownloaded.AutoSize = true;
+            this.labelDownloaded.Location = new System.Drawing.Point(218, 38);
+            this.labelDownloaded.Name = "labelDownloaded";
+            this.labelDownloaded.Size = new System.Drawing.Size(0, 13);
+            this.labelDownloaded.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 137);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Changelog";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(322, 111);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Changelog :\n\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 249);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelDownloaded);
+            this.Controls.Add(this.labelPerc);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Update";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelPerc;
+        private System.Windows.Forms.Label labelDownloaded;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Linq;
+using Ionic.Zip;
+
+/*Changelog*/
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        String version = "";
+        Stopwatch sw;
+        WebClient wc;
+        XElement xmlServeur;
+        String[] arg;
+        string currentVersion = "1.0";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            wc = new WebClient();
+            sw = new Stopwatch();
+
+            string versionUpdate = (string)(xmlServeur = XElement.Load("http://www.konosprod.fr/Updates/updater.xml").Element("version"));
+
+            if (versionUpdate != currentVersion)
+            {
+                MessageBox.Show("Une nouvelle version de l'updater est disponible, rendez-vous sur http://www.konosprod.fr/");
+                Environment.Exit(0);
+            }
+
+            if ((arg = Environment.GetCommandLineArgs()).Length != 3)
+            {
+                MessageBox.Show("Veuillez lancer la mise à jour via le programme que vous voulez mettre à jour !");
+                Environment.Exit(0);
+            }
+
+            loadVersion();
+            getVersionServer();
+
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            wc.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+            string test = (string)xmlServeur.Element("version");
+
+            if (test == version)
+            {
+                MessageBox.Show("Aucune mise à jour requise.", "Information");
+                Environment.Exit(0);
+            }
+            else
+            {
+                loadChangeLog();
+                update();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadChangeLog()
+        {
+            richTextBox1.Text += (string)xmlServeur.Element("changelog");        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.File.Delete("maj.zip");
+            Environment.Exit(0);
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+            labelSpeed.Text = (Convert.ToDouble(e.BytesReceived) / 1024 / sw.Elapsed.TotalSeconds).ToString("0.00") + " kb/s";
+            labelDownloaded.Text = (Convert.ToDouble(e.BytesReceived) / 1024 / 1024).ToString("0.00") + " Mo" + "  /  " + (Convert.ToDouble(e.TotalBytesToReceive) / 1024 / 1024).ToString("0.00") + " Mo";
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                System.IO.File.Delete("maj.zip");
+                Environment.Exit(0);
+            }
+            labelSpeed.Text = "";
+            sw.Stop();
+            extractFiles();
+            cancelButton.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void extractFiles()
+        {
+            labelPerc.Text = "Extracting files...";
+            ZipFile zf = ZipFile.Read("maj.zip");
+            System.IO.File.Move(arg[2], arg[2] + ".old");
+
+            foreach (ZipEntry ze in zf)
+            {
+                ze.Extract(".", ExtractExistingFileAction.OverwriteSilently);
+            }
+            zf.Dispose();
+            System.IO.File.Move(arg[1] + ".exe", arg[2]);
+            System.IO.File.Delete(arg[2] + ".old");
+            File.WriteAllText("version.txt", (string)xmlServeur.Element("version"));
+            labelPerc.Text = "Done";
+        }
+
+        private void update()
+        {
+            sw.Start();
+            wc.DownloadFileAsync(new Uri((string)xmlServeur.Element("url")), "maj.zip");
+        }
+
+        private void loadVersion()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("version.txt");
+                version = sr.ReadLine();
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                if (e is System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("Le fichier version.txt n'existe pas.", "Erreur");
+                }
+
+                Environment.Exit(-1);
+            }
+        }
+
+        private void getVersionServer()
+        {
+            xmlServeur = XElement.Load("http://www.konosprod.fr/Updates/"+ arg[1] +".xml");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wc.CancelAsync();
+        }
+    }
+}
